075c2b7fd9c7676669e3bf69d2ed104e7d59b767
Sun Haiwei <2946703196@qq.com>
2026-07-17 13:02:43 +0800

    Add factor recipes package

---
+++ factor_engine/factor_recipes/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Reusable factor recipes composed from primitive operators."""
+from factor_recipes.fundamental_ratios import (
+    current_ratio,
+    debt_to_equity,
+    operating_margin,
+    quick_ratio,
+)
+
+__all__ = [
+    "current_ratio",
+    "quick_ratio",
+    "debt_to_equity",
+    "operating_margin",
+]
