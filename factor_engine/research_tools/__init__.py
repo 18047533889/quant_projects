6e857d09cf047de370ab2d6a3b412e3e25988614
Sun Haiwei <2946703196@qq.com>
2026-07-17 15:24:39 +0800

    Add research tools package

---
+++ factor_engine/research_tools/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Research-only analytics removed from the production Factor DSL."""
+from research_tools.registry import ResearchToolRegistry
+
+__all__ = ["ResearchToolRegistry"]
