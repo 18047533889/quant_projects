1f2bc0e18163cb42b92d5c6f327784d88c2a3046
Sun Haiwei <2946703196@qq.com>
2026-07-19 03:17:00 +0800

    Load final operator audit fixes before governance

--- factor_engine/cleaned_operators/__init__.py
+++ factor_engine/cleaned_operators/__init__.py
@@ -52,6 +52,7 @@ _LOAD_MODULES = (
     "cleaned_operators.composite_fastpath_fixes",
     "cleaned_operators.layer_primitives",
     "cleaned_operators.layer_composite_fixes",
+    "cleaned_operators.operator_audit_fixes",
 )
 
 
