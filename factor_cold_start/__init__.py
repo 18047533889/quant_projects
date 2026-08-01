e5559eb48348f801fe6fe7426a0dd61a82db61d6
Sun Haiwei <2946703196@qq.com>
2026-08-01 23:43:22 +0800

    Expose production-aware cold-start APIs

--- factor_cold_start/__init__.py
+++ factor_cold_start/__init__.py
@@ -1,6 +1,32 @@
 """FactorEngine-native cold-start factor library."""
-from .catalog import filter_catalog, load_all_catalogs, load_catalog
+from .catalog import (
+    filter_catalog,
+    load_all_catalogs,
+    load_candidate_catalog,
+    load_catalog,
+    load_production_catalog,
+)
 from .model import ColdStartFactor
+from .production_admission import (
+    ProductionAdmission,
+    admit_factor,
+    admit_formula,
+    filter_production_factors,
+    rejected_production_factors,
+)
 from .sampler import sample_factors
 
-__all__ = ["ColdStartFactor", "load_catalog", "load_all_catalogs", "filter_catalog", "sample_factors"]
+__all__ = [
+    "ColdStartFactor",
+    "ProductionAdmission",
+    "load_catalog",
+    "load_production_catalog",
+    "load_candidate_catalog",
+    "load_all_catalogs",
+    "filter_catalog",
+    "sample_factors",
+    "admit_formula",
+    "admit_factor",
+    "filter_production_factors",
+    "rejected_production_factors",
+]
